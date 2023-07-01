ebf14841a9d13b61bfaec72d57a7d3337f13941b
ddecoen <ddecoen@gmail.com>
2023-06-30 20:12:35 -0700

    Update code to work on just the first dataset

--- Assignment1/assignment1.go
+++ Assignment1/assignment1.go
@@ -5,33 +5,43 @@ import (
 	"fmt"
 
 	"github.com/montanaflynn/stats"
+
+	"time"
 )
 
-type Coordiante struct {
-	X, Y float64
+//Create a variable to input the data
+var (
+	//Anscombe's Quartet Dataset I
+	x1 = []float64{10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5}
+	y1 = []float64{8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68}
+)
+
+//use Stats package to Coordinate data from the arrays
+func makeStatsData() []stats.Coordinate {
+	data1 := make([]stats.Coordinate, len(x1))
+
+	for i := 0; i < len(x1); i++ {
+		data1[i] = stats.Coordinate{X: x1[i], Y: y1[i]}
+	}
+
+	return data1
+}
+
+//use Stats package to produce the slope and intercepts
+func produceAllResults() error {
+	data1 := makeStatsData()
+
+	_, _, err := produce
 }
 
 func main() {
-	//Anscombe's Quarter Dataset I
-	data := []stats.Coordinate{
-		{10.0, 8.04},
-		{8.0, 6.95},
-		{13.0, 7.58},
-		{9.0, 8.81},
-		{11.0, 8.33},
-		{14.0, 9.96},
-		{6.0, 7.24},
-		{4.0, 4.26},
-		{12.0, 10.84},
-		{7.0, 4.82},
-		{5.0, 5.68},
+	//See how long the code takes to run
+	startTime := time.Now()
+	err := produceAllResults()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
 	}
-
-	slope, intercept := stats.LinearRegression(data)
-
-	fmt.Println("Regression results:")
-	fmt.Printf("Intercept: %0.2f\n", intercept)
-	fmt.Printf("Slope: %0.2f\n", slope)
-	//r, _ := stats.LinearRegression(data)
-	//fmt.Println(r)
+	endTime := time.Now()
+	timeElapsed := endTime.Sub(startTime)
+	fmt.Printf("Time running regressions: %v\n", timeElapsed)
 }
